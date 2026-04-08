a8eb7bae227a494fcffa1aff29519ed1f2f054c8
naftali <48069590+naftali100@users.noreply.github.com>
2026-04-09 01:40:26 +0300

    vp fmt: single quote

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite-plus";
+import { defineConfig } from 'vite-plus';
 
 export default defineConfig({
   staged: {
-    "*": "vp check --fix",
+    '*': 'vp check --fix',
   },
   lint: { options: { typeAware: true, typeCheck: true } },
   fmt: {
     printWidth: 80,
+    singleQuote: true,
   },
 });
